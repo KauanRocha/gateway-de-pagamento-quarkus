9cad39d5793dcbf649ede2a1256699c91d7f37d4
Kauan Rodrigues Oliveira Rocha <kauan.rocha@yuzer.com.br>
2024-05-13 19:47:42 -0300

    Fix tests

--- src/main/java/br/com/gateway/services/AbstractService.java
+++ src/main/java/br/com/gateway/services/AbstractService.java
@@ -5,7 +5,7 @@ import br.com.gateway.services.factories.DataBaseFactory;
 import br.com.gateway.services.factories.PaymentFactory;
 import jakarta.inject.Inject;
 
-public abstract class  AbstractService {
+public abstract class AbstractService {
 
     @Inject
     DataBaseFactory dataBaseFactory;
--- src/test/java/br/com/payment/GreetingResourceIT.java
+++ src/test/java/br/com/payment/GreetingResourceIT.java
@@ -1,4 +1,4 @@
-package org.acme;
+package br.com.payment;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
